dd8b0e8d0a3037c699cb5859f90d3db0d2a7f9fa
David <david.simonyants@gmail.com>
2022-07-23 13:37:40 +0400

    CURRENT/NO_CURRENT

--- esp32-host/main/main.c
+++ esp32-host/main/main.c
@@ -9,41 +9,45 @@
 #define GPIO_10 GPIO_NUM_33
 #define GPIO_11 GPIO_NUM_32
 
+#define CURRENT 1
+#define NO_CURRENT 0
+
 
 int app_main(void)
 {
+    wifi_start();
+    struct sockaddr_in name;
+    socklen_t name_len;
+    int server_socket = setup_server(1234, 300000, &name, &name_len);
+
     gpio_set_direction(GPIO_00, GPIO_MODE_OUTPUT);
     gpio_set_direction(GPIO_01, GPIO_MODE_OUTPUT);
     gpio_set_direction(GPIO_10, GPIO_MODE_OUTPUT);
     gpio_set_direction(GPIO_11, GPIO_MODE_OUTPUT);
 
-    wifi_start();
-    struct sockaddr_in name;
-    socklen_t name_len;
-    int server_socket = setup_server(1234, 300000, &name, &name_len);
     while (1)
     {
         signed char signal = acquire_signal(server_socket, &name, &name_len);
         if (signal == 1)
         {
-            gpio_set_level(GPIO_00, 1);
-            gpio_set_level(GPIO_01, 1);
-            gpio_set_level(GPIO_10, 0);
-            gpio_set_level(GPIO_11, 0);
+            gpio_set_level(GPIO_00, CURRENT);
+            gpio_set_level(GPIO_01, CURRENT);
+            gpio_set_level(GPIO_10, NO_CURRENT);
+            gpio_set_level(GPIO_11, NO_CURRENT);
         }
         else if (signal == -1)
         {
-            gpio_set_level(GPIO_00, 0);
-            gpio_set_level(GPIO_01, 0);
-            gpio_set_level(GPIO_10, 1);
-            gpio_set_level(GPIO_11, 1);
+            gpio_set_level(GPIO_00, NO_CURRENT);
+            gpio_set_level(GPIO_01, NO_CURRENT);
+            gpio_set_level(GPIO_10, CURRENT);
+            gpio_set_level(GPIO_11, CURRENT);
         }
         else
         {
-            gpio_set_level(GPIO_00, 0);
-            gpio_set_level(GPIO_01, 0);
-            gpio_set_level(GPIO_10, 0);
-            gpio_set_level(GPIO_11, 0);
+            gpio_set_level(GPIO_00, NO_CURRENT);
+            gpio_set_level(GPIO_01, NO_CURRENT);
+            gpio_set_level(GPIO_10, NO_CURRENT);
+            gpio_set_level(GPIO_11, NO_CURRENT);
         }
 
         printf("%d\n", signal);
